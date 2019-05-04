b83ebeb00190bbffb831454229dfccb860d463fc
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2019-05-04 18:47:50 +0200

    issue AB#2: Add initial State test project.
    - Add PacManState.Test project.
    - Add hello world test cases to this project.

---
+++ PacManState.Test/PacManState.Test.cpp
@@ -0,0 +1,6 @@
+// PacManState.Test.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#define CATCH_CONFIG_MAIN // Catch2 Main function provider
+#include "catch.hpp"
