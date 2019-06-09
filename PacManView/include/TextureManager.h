5aee7db6b9a086963223cac61f9625cd2200fdc6
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2019-06-09 17:03:06 +0200

    AB#9: Update TextureManager documentation

--- PacManView/include/TextureManager.h
+++ PacManView/include/TextureManager.h
@@ -9,23 +9,73 @@
 #include "renderer/IRenderer.h"
 #include "texture/ITexture.h"
 
+
 namespace pacman {
 namespace view {
 
+/// <summary>
+/// TextureManager is responsible for loading all <see cref="ITexture" /> from
+/// the file system. It provides the methods to load textures as well as query
+/// the <see cref="TextureManager /> has already loaded said textures.
+/// 
+/// All <see cref="ITexture" /> used within this project should be constructed
+/// through this <see cref="TextureManager" />. This will allow the manager to
+/// properly control the lifetime of all <see cref="ITexture" />. 
+/// 
+/// All textures are destroyed upon destroying this manager.
+/// </summary>
 class TextureManager {
-public:
-	TextureManager(const IRenderer& p_renderer);
-
-	void loadTexture(const std::string& file_path);
+public:	
+  /// <summary>
+  /// Construct a new <see cref="TextureManager"/> with the specified
+  /// <paramref name="p_renderer" />.
+  /// </summary>
+  /// <param name="p_renderer">The p renderer.</param>
+  TextureManager(const IRenderer& p_renderer);
 	
-	bool hasTexture(const std::string& file_path) const;
+  /// <summary>
+  /// Loads the <see cref="ITexture" /> at the specified 
+  /// <paramref name="file_path" />.
+  /// </summary>
+  /// <param name="file_path">The file path.</param>
+  /// <post-condition> 
+  /// (new this)-><see cref="getTexture" />(<paramref name="file_path" /> 
+  /// </post-condition>
+  void loadTexture(const std::string& file_path);
+	
+  /// <summary>
+  /// Determines whether the specified the <see cref="ITexture /> at the 
+  /// specified <paramref name="file_path" /> has already been loaded by this
+  /// <see cref="TextureManager" />.
+  /// </summary>
+  /// <param name="file_path"> The file path. </param>
+  /// <returns>
+  ///   <c>true</c> if the specified file_path has already been loaded; 
+  ///   otherwise, <c>false</c>.
+  /// </returns>
+  bool hasTexture(const std::string& file_path) const;
+	
+  /// <summary>
+  /// Get the <see cref="ITexture /> associated with the specified 
+  /// <paramref name="file_path" />.
+  /// </summary>
+  /// <param name="file_path"> The file path. </param>
+  /// <returns> 
+  /// The <see cref="ITexture" /> associated with the specified 
+  /// <paramref name="file_path" />. </returns>
+  const ITexture& getTexture(const std::string& file_path) const;
 
-	const ITexture& getTexture(const std::string& file_path) const;
-
-private:
-	std::map<std::string, std::unique_ptr<ITexture>> texture_map;
-
-	const IRenderer& renderer;
+private:  
+  /// <summary>
+  /// The texture map containing the pointers to the loaded 
+  /// <see cref="ITexture" /> associated with the path they belong to.
+  /// </summary>
+  std::map<std::string, std::unique_ptr<ITexture>> texture_map;
+  
+  /// <summary>
+  /// A reference to renderer used to load textures.
+  /// </summary>
+  const IRenderer& renderer;
 };
 
 } // view
