243e398359c59a8bd28a292ebb42e50f9c184cc1
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2019-06-09 17:12:44 +0200

    AB9: Update SpriteAnimation logic and doc.
    - Add additional time functions, including reset, set, get to
      SpriteAnimation.
    - Improve documentation of SpriteAnimation.

--- PacManView/include/SpriteAnimation.h
+++ PacManView/include/SpriteAnimation.h
@@ -5,9 +5,18 @@
 
 #include "Sprite.h"
 
+
 namespace pacman {
 namespace view {
 
+/// <summary>
+/// The SpriteAnimation provides a container for multiple sprites which are
+/// played with a specified time per frame. The SpriteAnimation can be updated
+/// with a dTime. This will move the internal clock forward.
+/// When <see cref="getActiveSprite" /> is retrieved, the currently active
+/// <see cref="Sprite" /> is retrieved based upon the time per frame and the
+/// current internally stored time.
+/// </summary>
 class DllExport SpriteAnimation {
 public:	
 	/// <summary>
@@ -17,33 +26,63 @@ public:
 	/// <param name="sprites"> The set of sprites to be rendered as a sequence. </param>
 	SpriteAnimation(float time_per_frame,
 					const std::vector<std::reference_wrapper<const Sprite>>& sprites);
-	
+
 	/// <summary>
 	/// Update this SpriteAnimation with the specified delta time.
 	/// </summary>
 	/// <param name="d_time">The d time.</param>
-	void update(float d_time);
+	/// <post-condition>
+	///   (new this)->getCurrentTime() == this->getCurrentTime() + d_time
+	/// </post-condition>
+	void updateTime(float d_time);
 	
 	/// <summary>
-	/// Get the current active sprite of this animation.
+	/// Resets the time of this SpriteAnimation back to zero.
 	/// </summary>
-	/// <returns> The current active sprite. </returns>
-	const Sprite& getActiveSprite() const;
+	/// <post-condition>
+	///   (new this)->getCurrentTime() == 0.0
+	/// </post-condition>
+	void resetTime() { this->cur_time = 0.f; }
 	
 	/// <summary>
-	/// Create a deep clone of this <see cref="SpriteAnimation" />.
+	/// Get the current time of this <see cref="SpriteAnimation" />.
 	/// </summary>
-	/// <returns> A deep clone of this <see cref="SpriteAnimation" />. </returns>
-	SpriteAnimation DeepClone() const;
+	/// <returns> The current time of this <see cref="SpriteAnimation" />. </returns>
+	inline float getCurrentTime() const { return this->cur_time; }
+
+  /// <summary>
+  /// Sets the current time to the specified time.
+  /// </summary>
+  /// <param name="new_time">The new time.</param>
+  /// <post-condition>
+  /// (new this)->getCurrentTime() == <paramref name="new_time />
+  /// </post-condition>
+  inline void setCurrentTimeTo(float new_time) { this->cur_time = new_time; }
+	
+  /// <summary>
+  /// Get the current active sprite of this animation.
+  /// </summary>
+  /// <returns> The current active sprite. </returns>
+  const Sprite& getActiveSprite() const;
+	
+  /// <summary>
+  /// Create a deep clone of this <see cref="SpriteAnimation" />.
+  /// </summary>
+  /// <returns> A deep clone of this <see cref="SpriteAnimation" />. </returns>
+  SpriteAnimation DeepClone() const;
 
 private:	
-	/// <summary> The set of sprites of this animation. </summary>
-	const std::vector<std::reference_wrapper<const Sprite>> animation_sprites;	
-	/// <summary> The time per frame. </summary>
-	const float time_per_frame;
+  /// <summary> The set of sprites of this animation. </summary>
+  const std::vector<std::reference_wrapper<const Sprite>> animation_sprites;	
 
-	/// <summary> The current time of this animation. </summary>
-	float cur_time;
+  /// <summary> The time per frame. </summary>
+  const float frames_per_second;
+
+  /// <summary> The current time of this animation. </summary>
+  float cur_time;
+  
+  /// <summary> The n frames of this <see cref="SpriteAnimation />. </summary>
+  const int n_frames;
 };
 
 }
--- PacManView/src/SpriteAnimation.cpp
+++ PacManView/src/SpriteAnimation.cpp
@@ -8,13 +8,18 @@ namespace view {
 
 SpriteAnimation::SpriteAnimation(float time_per_frame,
 								 const std::vector<std::reference_wrapper<const Sprite>>& sprites) :
-	time_per_frame(time_per_frame), 
+	frames_per_second(1.0 / time_per_frame), 
 	animation_sprites(std::vector<std::reference_wrapper<const Sprite>>(sprites)),
+	n_frames(sprites.size()),
 	cur_time(0.0F) { }
 
 
-void SpriteAnimation::update(float d_time) {
-	this->cur_time = fmod(cur_time + (d_time / this->time_per_frame), this->animation_sprites.size());
+void SpriteAnimation::updateTime(float d_time) {
+	this->cur_time += (d_time * this->frames_per_second);
+
+	if (cur_time > this->n_frames) {
+		cur_time = fmod(cur_time, this->n_frames);
+	}
 }
 
 
@@ -24,7 +29,7 @@ const Sprite& SpriteAnimation::getActiveSprite() const {
 
 
 SpriteAnimation SpriteAnimation::DeepClone() const {
-	return SpriteAnimation(this->time_per_frame,
+	return SpriteAnimation(1.0 / this->frames_per_second,
 						   this->animation_sprites);
 }
 
