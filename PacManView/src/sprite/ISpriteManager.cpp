55a58b972defba52eafc384bfc11fe5ac438504d
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2019-06-15 10:12:14 +0200

    AB#9: Add ISpriteManager constructor

---
+++ PacManView/src/sprite/ISpriteManager.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "sprite/ISpriteManager.h"
+#include "sprite/SpriteManager.h"
+
+namespace pacman {
+namespace view {
+
+std::unique_ptr<ISpriteManager> ISpriteManager::construct(const ITextureManager& texture_manager) {
+  return std::make_unique<SpriteManager>(texture_manager);
+}
+
+}
+}
