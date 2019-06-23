e59745bc119c0b898a97350b0cc2ac64360fe5f9
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2019-06-23 15:34:33 +0200

    AB#9: Update TextureMock to use correct const call

--- PacManView.Test/include/TextureManagerMock.h
+++ PacManView.Test/include/TextureManagerMock.h
@@ -9,8 +9,8 @@ class TextureManagerMock : public pacman::view::ITextureManager {
 public:
 	MOCK_METHOD1(loadTexture,
 				 void(const std::string& file_path));
-	MOCK_METHOD1(hasTexture, 
-				 bool(const std::string& file_path));
+	MOCK_CONST_METHOD1(hasTexture, 
+					   bool(const std::string& file_path));
 
 	MOCK_CONST_METHOD1(getTexture,
 					   const pacman::view::ITexture&(const std::string& file_path));
