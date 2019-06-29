8b42b6bf1777a77be45f0f0404c865043f982251
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2019-06-29 15:04:31 +0200

    AB#19: Add test for screen size exception

---
+++ PacManView.Test/src/ViewCoreTest.cpp
@@ -0,0 +1,46 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "renderer/IViewCore.h"
+#include "exceptions/ViewException.h"
+
+
+namespace pacman {
+namespace view {
+
+struct ScreenDim {
+	ScreenDim(int x, int y) : x(x), y(y) { }
+
+	int x;
+	int y;
+};
+
+class InitialiseViewCorePatternTest : public ::testing::TestWithParam<ScreenDim> { };
+
+
+// Given an IViewCore
+//   and an invalid ScreenDim
+// When initialise is called
+// Then an error is thrown
+TEST_P(InitialiseViewCorePatternTest, initialise_WithInvalidScreenDimsThrowsException) {
+  // Given
+  std::unique_ptr<IViewCore> view_core = IViewCore::construct();
+  ScreenDim screen_dim = GetParam();
+
+  // When | Then
+  ASSERT_THROW(view_core->initialise(screen_dim.x, screen_dim.y),
+			   ViewException);
+}
+
+
+INSTANTIATE_TEST_SUITE_P(InitViewCore_InvalidScreenDimThrowException,
+						 InitialiseViewCorePatternTest,
+						 ::testing::Values(ScreenDim( -1, 100), 
+										   ScreenDim(  0, 100),
+										   ScreenDim(100,   0),
+										   ScreenDim(100,  -1),
+										   ScreenDim(  0,   0),
+										   ScreenDim( -1,  -1)));
+
+}
+}
