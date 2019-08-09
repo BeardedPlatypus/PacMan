e7db4924453a9106a60f8ba292728da7d09d27be
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2019-08-09 22:12:59 +0200

    AB#45: Add ScancodeConverter src

---
+++ PacManController/src/keyboard/ScancodeConverter.cpp
@@ -0,0 +1,34 @@
+#include "stdafx.h"
+#include "keyboard/ScancodeConverter.h"
+
+
+namespace pacman {
+namespace controller {
+namespace keyboard {
+
+ScancodeConverter::ScancodeConverter() : 
+    scancode_map(std::unordered_map<SDL_Scancode, Scancode>()) {
+  this->scancode_map[SDL_SCANCODE_W] = Scancode::W;
+  this->scancode_map[SDL_SCANCODE_A] = Scancode::A;
+  this->scancode_map[SDL_SCANCODE_S] = Scancode::S;
+  this->scancode_map[SDL_SCANCODE_D] = Scancode::D;
+
+  this->scancode_map[SDL_SCANCODE_ESCAPE] = Scancode::Escape;
+
+  this->scancode_map[SDL_SCANCODE_UP]    = Scancode::ArrowUp;
+  this->scancode_map[SDL_SCANCODE_LEFT]  = Scancode::ArrowLeft;
+  this->scancode_map[SDL_SCANCODE_DOWN]  = Scancode::ArrowDown;
+  this->scancode_map[SDL_SCANCODE_RIGHT] = Scancode::ArrowRight;
+}
+
+
+Scancode ScancodeConverter::Convert(SDL_Scancode sdl_scancode) const {
+  if (this->scancode_map.find(sdl_scancode) == this->scancode_map.end())
+    return Scancode::UnmappedCode;
+
+  return this->scancode_map.at(sdl_scancode);
+}
+
+}
+}
+}
