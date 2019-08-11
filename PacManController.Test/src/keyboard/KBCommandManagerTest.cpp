11286766c895a77e29b89f1e2e48512727ee88f8
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2019-08-11 18:04:01 +0200

    AB#45: Update naming KBCommandManagerTest

--- PacManController.Test/src/keyboard/KBCommandManagerTest.cpp
+++ PacManController.Test/src/keyboard/KBCommandManagerTest.cpp
@@ -4,7 +4,7 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-#include "keyboard/ICommandManager.h"
+#include "keyboard/IKBCommandManager.h"
 
 #pragma region using_statements
 using ::testing::Eq;
