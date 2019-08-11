8732dd878709281dde1bc176546d0ac224bd1f7a
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2019-08-11 18:05:59 +0200

    AB#45: Update ControllerManager

---
+++ PacManController/include/manager/ControllerManager.h
@@ -0,0 +1,161 @@
+#pragma once
+
+#include "manager/IControllerManager.h"
+#include "event/IEventGenerator.h"
+#include "event/IEventParser.h"
+#include "keyboard/IKBCommandManager.h"
+#include "system/ISystemCommandManager.h"
+#include "event/IEventStore.h"
+
+
+namespace pacman {
+namespace controller {
+
+class ControllerManager final : public IControllerManager {
+public:
+  /// <summary>
+  /// Construct a new <see cref="ControllerManager" /> with the given parameters.
+  /// </summary>
+  /// <param name="p_event_generator"> 
+  /// A pointer to the <see cref="IEventGenerator" />.
+  /// </param>
+  /// <param name="p_keyboard_command_manager">
+  /// A pointer to the <see cref="keyboard::ICommandManager" />.
+  /// <param name="p_system_command_manager">
+  /// A pointer to the <see cref="system::ICommandManager" />.
+  /// </param>
+  /// <param name="p_keyboard_event_parser">
+  /// A pointer to the <see cref="IEventParser<IKeyboardEvent>" />.
+  /// </param>
+  /// <param name="p_keyboard_event_parser">
+  /// A pointer to the <see cref="IEventParser<IKeyboardEvent>" />.
+  /// </param>
+  /// <param name="p_keyboard_event_parser">
+  /// A pointer to the <see cref="IEventParser<ISystemEvent>" />.
+  /// </param>
+  ControllerManager(std::unique_ptr<IEventGenerator> p_event_generator,
+                    std::unique_ptr<keyboard::ICommandManager> p_keyboard_command_manager,
+                    std::unique_ptr<system::ICommandManager> p_system_command_manager,
+                    std::unique_ptr<IEventParser<IKeyboardEvent>> p_keyboard_event_parser,
+                    std::unique_ptr<IEventParser<ISystemEvent>> p_system_event_parser,
+                    std::unique_ptr<IEventStore> p_event_store);
+
+  void Update() final;
+
+  void RegisterSystemCommand(SystemEventType system_event_type,
+                             std::unique_ptr<ICommand> p_command) final;
+
+  void DeregisterSystemCommand(SystemEventType system_event_type) final;
+
+  void RegisterKeyboardCommand(KeyboardEventType keyboard_event_type,
+                               keyboard::Scancode scancode,
+                               std::unique_ptr<ICommand> p_command) final;
+
+  void DeregisterKeyboardCommand(KeyboardEventType keyboard_event_type,
+                                 keyboard::Scancode scancode) final;
+
+private:
+  /// <summary>
+  /// Get a pointer to the <see cref="IEventGenerator" /> of this 
+  /// <see cref="ControllerManager" />.
+  /// </summary>
+  /// <returns> 
+  /// A pointer to the <see cref="IEventGenerator" /> of this 
+  /// <see cref="ControllerManager" />
+  /// </returns>
+  inline IEventGenerator* GetEventGenerator() {
+    return this->p_event_generator.get();
+  }
+
+  /// <summary>
+  /// The <see cref="IEventGenerator" /> of this <see cref="ControllerManager"/>.
+  /// </summary>
+  std::unique_ptr<IEventGenerator> p_event_generator;
+
+  /// <summary>
+  /// Get a pointer to the <see cref="keyboard::ICommandManager" /> of this 
+  /// <see cref="ControllerManager" />.
+  /// </summary>
+  /// <returns> 
+  /// A pointer to the <see cref="keyboard::ICommandManager" /> of this 
+  /// <see cref="ControllerManager" />
+  /// </returns>
+  inline keyboard::ICommandManager* GetKeyboardCommandManager() {
+    return this->p_keyboard_command_manager.get();
+  }
+
+  /// <summary>
+  /// The <see cref="keyboard::ICommandManager" /> of this <see cref="ControllerManager"/>.
+  /// </summary>
+  std::unique_ptr<keyboard::ICommandManager> p_keyboard_command_manager;
+
+  /// <summary>
+  /// Get a pointer to the <see cref="IEventParser<IKeyboardEvent>" /> of this 
+  /// <see cref="ControllerManager" />.
+  /// </summary>
+  /// <returns> 
+  /// A pointer to the <see cref="IEventParser<IKeyboardEvent" /> of this 
+  /// <see cref="ControllerManager" />
+  /// </returns>
+  inline IEventParser<IKeyboardEvent>* GetKeyboardEventParser() {
+    return this->p_keyboard_event_parser.get();
+  }
+
+  /// <summary>
+  /// The <see cref="IEventParser<IKeyboardEvent>" /> of this <see cref="ControllerManager"/>.
+  /// </summary>
+  std::unique_ptr<IEventParser<IKeyboardEvent>> p_keyboard_event_parser;
+
+  /// <summary>
+  /// Get a pointer to the <see cref="system::ICommandManager" /> of this 
+  /// <see cref="ControllerManager" />.
+  /// </summary>
+  // <returns> 
+  // A pointer to the <see cref="system::ICommandManager" /> of this 
+  // <see cref="ControllerManager" />
+  // </returns>
+  inline system::ICommandManager* GetSystemCommandManager() {
+    return this->p_system_command_manager.get();
+  }
+
+  /// <summary>
+  /// The <see cref="system::ICommandManager" /> of this <see cref="ControllerManager"/>.
+  /// </summary>
+  std::unique_ptr<system::ICommandManager> p_system_command_manager;
+
+  /// <summary>
+  /// Get a pointer to the <see cref="IEventParser<ISystemEvent>" /> of this 
+  /// <see cref="ControllerManager" />.
+  /// </summary>
+  /// <returns> 
+  /// A pointer to the <see cref="IEventGenerator" /> of this 
+  /// <see cref="ControllerManager" />.
+  /// </returns>
+  inline IEventParser<ISystemEvent>* GetSystemEventParser() {
+    return this->p_system_event_parser.get();
+  }
+
+  /// <summary>
+  /// The <see cref="IEventParser<ISystemEvent>" /> of this <see cref="ControllerManager"/>.
+  /// </summary>
+  std::unique_ptr<IEventParser<ISystemEvent>> p_system_event_parser;
+   
+  /// <summary>
+  /// Gets a pointer to the <see cref="IEventStore" /> of this <see cref="ControllerManager" />.
+  /// </summary>
+  /// <returns>
+  /// A pointer to the <see cref="IEventStore" /> of this 
+  /// <see cref="ControllerManager" />.
+  /// </returns>
+  inline IEventStore* GetEventStore() {
+    return this->p_event_store.get();
+  }
+
+  /// <summary>
+  /// The <see cref="IEventStore" /> of this <see cref="ControllerManager" />.
+  /// </summary>
+  std::unique_ptr<IEventStore> p_event_store;
+};
+
+}
+}
--- PacManController/include/manager/IControllerManager.h
+++ PacManController/include/manager/IControllerManager.h
@@ -4,6 +4,12 @@
 
 #include <memory>
 
+#include "event/ISystemEvent.h"
+#include "event/IKeyboardEvent.h"
+#include "ICommand.h"
+
+namespace pacman {
+namespace controller {
 
 /// <summary>
 /// <see cref="IControllerManager" /> provides an interface for external
@@ -21,5 +27,70 @@ public:
 	/// <see cref="IControllerManager" />.
 	/// </returns>
 	static std::unique_ptr<IControllerManager> construct();
+
+  virtual ~IControllerManager() {}
+
+  /// <summary>
+  /// Update the controller component.
+  /// </summary>
+  virtual void Update() = 0;
+
+  /// <summary>
+  /// Register the command of <paramref name="p_command" /> to the 
+  /// <see cref="ISystemEvent" /> of type <paramref name="system_event_type" />.
+  /// </summary>
+  /// <param name="system_event_type"> Type of the system event. </param>
+  /// <param name="p_command"> 
+  /// A <see cref="std::unique_ptr" /> to the command to be registered .
+  /// </param>
+  // <remarks>
+  /// If a command is already associated with the specified 
+  /// <paramref name="system_event_type" /> it will be overwritten.
+  /// </remarks>
+  virtual void RegisterSystemCommand(SystemEventType system_event_type,
+                                     std::unique_ptr<ICommand> p_command) = 0;
+
+  /// <summary>
+  /// Deregisters the command associated with <see cref="ISystemEvent" /> of
+  /// type <paramref name="system_event_type" />.
+  /// </summary>
+  /// <param name="system_event_type">Type of system event.</param>
+  /// <remarks>
+  /// If no event was registered, nothing will change.
+  /// </remarks>
+  virtual void DeregisterSystemCommand(SystemEventType system_event_type) = 0;
+
+  /// <summary>
+  /// Registers the command of <paramref name="p_command" /> to the 
+  /// <see cref="IKeyboardEvent" /> with type <paramref name="keyboard_event_type" />
+  /// and the specified <paramref name="scancode" />.
+  /// </summary>
+  /// <param name="keyboard_event_type">Type of the keyboard event.</param>
+  /// <param name="scancode">The scancode.</param>
+  /// <param name="p_command"> 
+  /// A <see cref="std::unique_ptr" /> to the command to be registered .
+  /// </param>
+  // <remarks>
+  /// If a command is already associated with the specified 
+  /// <paramref name="keyboard_event_type" /> and <paramref name="scancode" />
+  /// it will be overwritten.
+  /// </remarks>
+  virtual void RegisterKeyboardCommand(KeyboardEventType keyboard_event_type,
+                                       keyboard::Scancode scancode,
+                                       std::unique_ptr<ICommand> p_command) = 0;
+
+  /// <summary>
+  /// Deregisters the command associated with <see cref="IKeyboardEvent" /> of
+  /// type <paramref name="keyboard_event_type" /> and <paramref name="scancode" />.
+  /// </summary>
+  /// <param name="keyboard_event_type">Type of keyboard event.</param>
+  /// <param name="scancode">The scancode.</param>
+  /// <remarks>
+  /// If no event was registered, nothing will change.
+  /// </remarks>
+  virtual void DeregisterKeyboardCommand(KeyboardEventType keyboard_event_type,
+                                         keyboard::Scancode scancode) = 0;
 };
 
+}
+}
