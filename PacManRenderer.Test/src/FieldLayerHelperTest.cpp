aad4fa7b4a30683cfe694ae96cbb9cb8f3d96243
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2019-08-25 10:31:03 +0200

    AB#49: Add initial three FieldLayerHelperTests

--- PacManRenderer.Test/src/FieldLayerHelperTest.cpp
+++ PacManRenderer.Test/src/FieldLayerHelperTest.cpp
@@ -1,18 +1,86 @@
-#include "FieldMock.h"
-
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 #include "FieldLayerHelper.h"
 
+#include "field/IField.h"
+
+
+using ::testing::Eq;
+using ::testing::Return;
 
 namespace pacman {
 namespace renderer {
 
-struct FieldLayerHelperTestValues {
-};
+TEST(FieldLayerHelperTest, CalculateFieldValue_SingleSpace) {
+  // Given
+  std::vector<std::vector<state::field::TileType>> tiles = { {state::field::TileType::Space } };
+  std::vector<unsigned int> expected_result = { 0 };
+  std::unique_ptr<state::field::IField> p_field = state::field::IField::Construct(tiles);
+
+  // When
+  std::vector<unsigned int> result = 
+    FieldLayerHelper::CalculateFieldVisualisation(p_field.get());
+
+  // Then
+  ASSERT_THAT(result.size(), Eq(expected_result.size()));
+
+  for (auto i = 0; i < result.size(); ++i) {
+    ASSERT_THAT(result[i], Eq(expected_result[i]));
+  }
+}
 
 
+TEST(FieldLayerHelperTest, CalculateFieldValue_SingleSolid) {
+  // Given
+  std::vector<std::vector<state::field::TileType>> tiles = { {state::field::TileType::Solid } };
+  std::unique_ptr<state::field::IField> p_field = state::field::IField::Construct(tiles);
+
+  unsigned int result_value = 1;
+
+  for (int i = 2; i < 18; i++) {
+    result_value += 1 << i;
+  }
+
+  std::vector<unsigned int> expected_result = { result_value };
+
+  // When
+  std::vector<unsigned int> result = 
+    FieldLayerHelper::CalculateFieldVisualisation(p_field.get());
+
+  // Then
+  ASSERT_THAT(result.size(), Eq(expected_result.size()));
+
+  for (auto i = 0; i < result.size(); ++i) {
+    ASSERT_THAT(result[i], Eq(expected_result[i]));
+  }
+}
+
+
+TEST(FieldLayerHelperTest, CalculateFieldValue_SingleGate) {
+  // Given
+  std::vector<std::vector<state::field::TileType>> tiles = { {state::field::TileType::Gate } };
+  std::unique_ptr<state::field::IField> p_field = state::field::IField::Construct(tiles);
+
+  unsigned int result_value = 2;
+
+  for (int i = 2; i < 18; i++) {
+    result_value += 1 << i;
+  }
+
+  std::vector<unsigned int> expected_result = { result_value };
+
+  // When
+  std::vector<unsigned int> result = 
+    FieldLayerHelper::CalculateFieldVisualisation(p_field.get());
+
+  // Then
+  ASSERT_THAT(result.size(), Eq(expected_result.size()));
+
+  for (auto i = 0; i < result.size(); ++i) {
+    ASSERT_THAT(result[i], Eq(expected_result[i]));
+  }
+}
 
 }
 }
