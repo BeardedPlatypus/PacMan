1db06ca121154fb5c2d6e0dfe46cbf2784d89c50
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2019-09-28 12:48:37 +0200

    AB#48: Update documentation of IUpdatablePlayerEntity

--- PacManUpdate/include/entity/IUpdatablePlayerEntity.h
+++ PacManUpdate/include/entity/IUpdatablePlayerEntity.h
@@ -10,20 +10,75 @@
 
 namespace pacman {
 namespace update {
-
+  
+/// <summary>
+/// <see cref="IUpdatablePlayerEntity" /> wraps and extends a 
+/// <see cref="state::IEntityState" />.
+/// </summary>
 class DllExport IUpdatablePlayerEntity {
-public:
+public:  
+  /// <summary>
+  /// Construct a new <see cref="IUpdatablePlayerEntity" /> with the given 
+  /// <paramref name="p_player_state" />.
+  /// </summary>
+  /// <param name="p_player_state">
+  /// A pointer to the <see cref="state::IEntityState" /> it will wrap.
+  /// </param>
+  /// <returns>
+  /// A <see cref="std::unique_ptr"/> containing the new 
+  /// <see cref="IUpdatablePlayerEntity"/>.
+  /// </returns>
   static std::unique_ptr<IUpdatablePlayerEntity> Construct(state::IEntityState* p_player_state);
-
+  
+  /// <summary>
+  /// Get the x axis of this <see cref="IUpdatablePlayerEntity"/>.
+  /// </summary>
+  /// <returns> A pointer to the x axis.</returns>
   virtual IUpdatableEntityAxis* GetXAxis() const = 0;
+
+  /// <summary>
+  /// Get the y axis of this <see cref="IUpdatablePlayerEntity"/>.
+  /// </summary>
+  /// <returns> A pointer to the y axis.</returns>
   virtual IUpdatableEntityAxis* GetYAxis() const = 0;
-
+  
+  /// <summary>
+  /// Get the speed of this <see cref="IUpdatablePlayerEntity"/>.
+  /// </summary>
+  /// <returns>The speed.</returns>
   virtual float GetSpeed() const = 0;
+  
+  /// <summary>
+  /// Get the current moving direction of this 
+  /// <see cref="IUpdatablePlayerEntity" />.
+  /// </summary>
+  /// <returns>The current moving direction.</returns>
+  virtual state::Direction GetMovingDirection() const = 0;  
 
-  virtual state::Direction GetMovingDirection() const = 0;
+  /// <summary>
+  /// Set the current moving direction of this
+  /// <see cref="IUpdatablePlayerEntity" /> to <paramref name="direction" />.
+  /// </summary>
+  /// <param name="direction">The direction.</param>
+  /// <postcondition>
+  /// | (new this)->GetMovingDirection() == direction;
+  /// </postcondition
   virtual void SetMovingDirection(state::Direction direction) = 0;
-
+  
+  /// <summary>
+  /// Get the next direction of this <see cref="IUpdatablePlayerEntity" />.
+  /// </summary>
+  /// <returns>The next direction.</returns>
   virtual std::optional<state::Direction> GetNextDirection() const = 0;
+  
+  /// <summary>
+  /// Set the next direction of this <see cref="IUpdatablePlayerEntity" /> 
+  /// to the specified <paramref name="direction"/>.
+  /// </summary>
+  /// <param name="direction">The direction.</param>
+  /// <postcondition>
+  /// | (new this)->GetNextDirection() == direction;
+  /// </postcondition
   virtual void SetNextDirection(std::optional<state::Direction> direction) = 0;
 };
 
