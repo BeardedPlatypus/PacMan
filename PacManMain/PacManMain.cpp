77a310cecc0b14f74f7ae7aafdc0c83b01844972
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2019-12-28 11:23:21 +0100

    AB#97: Update PacManMain with PacManViewAPI

--- PacManMain/PacManMain.cpp
+++ PacManMain/PacManMain.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 
 #include <GameState.h>
-#include <manager/IViewManager.h>
+#include <IViewAPI.h>
 #include <manager/IControllerManager.h>
 #include <manager/IRendererManager.h>
 #include <manager/IUpdateManager.h>
@@ -17,61 +17,6 @@
 #include <SDL2/SDL.h>
 
 #undef main // Required for SDL2
-
-
-void viewConfiguration(pacman::view::IViewManager& manager) {
-
-	std::string file_path = "./assets/characters.png";
-
-	manager.requestSprite("test1", file_path,  pacman::view::ClipRect(0, 32, 16, 16));
-	manager.requestSprite("test2", file_path, pacman::view::ClipRect(16, 32, 16, 16));
-
-	manager.requestSprite("test3", file_path,  pacman::view::ClipRect(0, 48, 16, 16));
-	manager.requestSprite("test4", file_path, pacman::view::ClipRect(16, 48, 16, 16));
-
-	manager.requestSprite("test5", file_path,  pacman::view::ClipRect(0, 64, 16, 16));
-	manager.requestSprite("test6", file_path, pacman::view::ClipRect(16, 64, 16, 16));
-
-	manager.requestSprite("test7", file_path,  pacman::view::ClipRect(0, 80, 16, 16));
-	manager.requestSprite("test8", file_path, pacman::view::ClipRect(16, 80, 16, 16));
-
-	manager.requestSprite("p1", file_path,  pacman::view::ClipRect(0, 0, 16, 16));
-	manager.requestSprite("p2", file_path, pacman::view::ClipRect(16, 0, 16, 16));
-	manager.requestSprite("p3", file_path, pacman::view::ClipRect(32, 0, 16, 16));
-
-	auto sprites1 = std::vector<std::string>();
-	sprites1.push_back("test1");
-	sprites1.push_back("test2");
-
-	manager.requestSpriteAnimation("anim1", 0.3F, sprites1);
-
-	auto sprites2 = std::vector<std::string>();
-	sprites2.push_back("test3");
-	sprites2.push_back("test4");
-
-	manager.requestSpriteAnimation("anim2", 0.3F, sprites2);
-
-	auto sprites3 = std::vector<std::string>();
-	sprites3.push_back("test5");
-	sprites3.push_back("test6");
-
-	manager.requestSpriteAnimation("anim3", 0.3F, sprites3);
-
-	auto sprites4 = std::vector<std::string>();
-	sprites4.push_back("test7");
-	sprites4.push_back("test8");
-
-	manager.requestSpriteAnimation("anim4", 0.3F, sprites4);
-
-	auto sprites5 = std::vector<std::string>();
-	sprites5.push_back("p1");
-	sprites5.push_back("p2");
-	sprites5.push_back("p3");
-	sprites5.push_back("p2");
-
-	manager.requestSpriteAnimation("anim5", 0.175F, sprites5);
-}
-
 
 int main(int argc, char **argv) {
   if (argc != 1) return 1;
@@ -86,11 +31,11 @@ int main(int argc, char **argv) {
                                                                2.5F);
   p_game_state->SetPlayerState(std::move(p_player_state));
 
-  auto view_manager = pacman::view::IViewManager::construct();
-  view_manager->initialise(800, 1000);
+  auto view_api = pacman::view::IViewAPI::Construct();
+  view_api->Initialise(800, 1000);
 
   auto p_renderer_manager = pacman::renderer::IRendererManager::Construct(p_game_state.get(),
-                                                                          view_manager.get());
+                                                                          view_api.get());
   p_renderer_manager->Initialise();
 
   auto p_controller_manager = pacman::controller::IControllerManager::Construct();
@@ -116,11 +61,9 @@ int main(int argc, char **argv) {
     p_update_manager->Update(dtime);
 	  //   Update current state based on updated behaviour.
 	  //   Render the current state.
-	  view_manager->update(dtime);
-
-    view_manager->initialiseRender();
+    view_api->InitialiseRender();
     p_renderer_manager->Render();
-    view_manager->finaliseRender();
+    view_api->FinaliseRender();
 
 	  //   Play music or anything like that.
   }
