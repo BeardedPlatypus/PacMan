399748b9959a67989f316eed7fad5c9e580683ed
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2019-12-29 11:11:13 +0100

    AB#97: Add PacManUpdateAPI

---
+++ PacManUpdateAPI/include/UpdateAPI.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "IUpdateAPI.h"
+
+#include "entity/IUpdatablePlayerEntity.h"
+
+
+namespace pacman {
+namespace update {
+
+class UpdateAPI final : public IUpdateAPI {
+public:
+  UpdateAPI(state::IGameState* p_game_state,
+            controller::api::IControllerAPI* p_controller_manager);
+
+  void Initialise() final;
+  void Update(float dl) final;
+
+private:  
+  /// <summary>
+  /// Initialise the commands within the <see cref="controller::IControllerManager" />.
+  /// </summary>
+  void InitialiseCommands();
+  
+  /// <summary>
+  /// A pointer to the <see cref="IGameState" />
+  /// </summary>
+  state::IGameState* p_game_state;
+  
+  /// <summary>
+  /// A pointer to the <see cref="controller::IControllerManager" />.
+  /// </summary>
+  controller::api::IControllerAPI* p_controller_manager;
+
+  std::unique_ptr<IUpdatablePlayerEntity> p_player_entity;
+};
+
+}
+}
