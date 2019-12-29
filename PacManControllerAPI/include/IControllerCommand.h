2119d39a62e4611c7a5993277cb230e960391d51
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2019-12-29 09:09:22 +0100

    AB#97: Update documentation IControllerCommand

--- PacManControllerAPI/include/IControllerCommand.h
+++ PacManControllerAPI/include/IControllerCommand.h
@@ -5,9 +5,16 @@
 namespace pacman {
 namespace controller {
 namespace api {
-
+	
+/// <summary>
+/// <see cref="IControllerCommand"/> defines the interface of commands which
+/// can be registered within the <see cref="IControllerAPI"/>.
+/// </summary>
 class DllExport IControllerCommand {
-public:
+public:	
+	/// <summary>
+	/// Finalise this <see cref="IControllerCommand"/>.
+	/// </summary>
 	virtual ~IControllerCommand() = default;
 
 	/// <summary>
