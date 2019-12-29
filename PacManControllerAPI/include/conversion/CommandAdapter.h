426d08760c99da391ac3a30628515d8502b4e110
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2019-12-29 09:20:01 +0100

    AB#97: Add Documentation to CommandAdapter.

--- PacManControllerAPI/include/conversion/CommandAdapter.h
+++ PacManControllerAPI/include/conversion/CommandAdapter.h
@@ -1,21 +1,35 @@
 #pragma once
 
+#include <memory>
+
 #include "ICommand.h"
 #include "IControllerCommand.h"
 
-#include <memory>
 
 namespace pacman {
 namespace controller {
 namespace api {
 
+/// <summary>
+/// <see cref="CommandAdapter"/> acts as a wrapper to ensure the external
+/// <see cref="IControllerCommand"/> can be used as an internal 
+/// <see cref="ICommand"/>.
+/// </summary>
+/// <seealso cref="ICommand"/>
 class CommandAdapter final : public ICommand {
-public:
+public:  
+  /// <summary>
+  /// Create a new <see cref="CommandAdapter"/>.
+  /// </summary>
+  /// <param name="p_cmd">The p command.</param>
   explicit CommandAdapter(std::unique_ptr<IControllerCommand> p_cmd);
 
   inline void Execute() final { this->p_cmd->Execute(); }
 
-private:
+private:  
+  /// <summary>
+  /// A pointer to the command this <see cref="CommandAdapter"/> wraps.
+  /// </summary>
   std::unique_ptr<IControllerCommand> p_cmd;
 };
 
