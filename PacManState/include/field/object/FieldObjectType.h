1ef1af172d92a5a243c91565f8849b35f8bc318d
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2020-01-05 20:06:56 +0100

    AB#74: Update FieldObjectType documentation.

--- PacManState/include/field/object/FieldObjectType.h
+++ PacManState/include/field/object/FieldObjectType.h
@@ -6,11 +6,21 @@ namespace pacman {
 namespace state {
 namespace field {
 
+/// <summary>
+/// <see cref="FieldObjectType"/> describes the different types of field
+/// objects with which entities can interact.
+/// </summary>
 enum class DllExport FieldObjectType {
   Portal = 1,
 };
 
 
+/// <summary>
+/// Check whether the specified <paramref name="t"/> is defined.
+/// </summary>
+/// <param name="t"> The type to check. </param>
+/// <returns>
+/// <c>true</c> if <paramref name="t"/> is defined; otherwise <c>false</c>.
 bool IsDefined(FieldObjectType t);
 
 }
