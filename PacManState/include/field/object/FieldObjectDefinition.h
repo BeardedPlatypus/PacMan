0cfc8544e38c662249f0fadc5904adcd3dca7189
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2020-01-06 19:30:18 +0100

    AB#74: Add FieldObjectDefinition documentation

--- PacManState/include/field/object/FieldObjectDefinition.h
+++ PacManState/include/field/object/FieldObjectDefinition.h
@@ -8,6 +8,11 @@ namespace pacman {
 namespace state {
 namespace field {
 
+/// <summary>
+/// Get the field objects defined on the field as a 2D vector, 
+/// where each entry corresponds with either an object, or an 
+/// undefined value.
+/// </summary>
 DllExport std::vector<std::vector<FieldObjectType>> GetFieldObjectDefinition();
 
 }
