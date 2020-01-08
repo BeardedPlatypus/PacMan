ac59eaca8a4a6d461c23d8c2726c1eb99c76a345
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2020-01-08 06:10:31 +0100

    AB#74: Add documentation to PortalBehaviour

--- PacManUpdate/include/objects/behaviours/PortalBehaviour.h
+++ PacManUpdate/include/objects/behaviours/PortalBehaviour.h
@@ -10,12 +10,40 @@ namespace pacman {
 namespace update {
 namespace object {
 
+/// <summary>
+/// <see cref="PortalBehaviour"/> defines the behaviour which transports an
+/// entity from one location, defined as a FieldIndex, to another.
+/// </summary>
+/// <seealso cref="IObjectBehaviour" />
 class PortalBehaviour : public IObjectBehaviour {
-public:
+public:  
+  /// <summary>
+  /// Creates a new <see cref="PortalBehaviour"/>.
+  /// </summary>
+  /// <param name="p_field"> 
+  /// The field on which this behaviour is created. 
+  /// </param>
+  /// <param name="mapping">
+  /// The portal mapping locations.
+  /// </param>
+  // <remarks>
+  // The mappings are defined one way, as such, if a mapping A -> B
+  // exist, and B -> A also needs to exist, it needs to be explicitly
+  // defined.
+  // </remarks>
   PortalBehaviour(const state::field::IField* p_field,
                   const std::unordered_map<int, int> mapping);
-
+  
+  /// <summary>
+  /// If a portal exists at (<paramref name="x"/>, <paramref name="y"/>),
+  /// then portal <paramref name="p_entity"/> to its corresponding location
+  /// else nothing.
+  /// </summary>
+  /// <param name="p_entity">The <see cref="IUpdatablePlayerEntity" /> on which this behaviour should act.</param>
+  /// <param name="x">The x location at which this behaviour is executed.</param>
+  /// <param name="y">The y location at which this behaviour is executed.</param>
   void Execute(IUpdatablePlayerEntity* p_entity, int x, int y) override;
+
 private:
   const state::field::IField* _p_field;
 
