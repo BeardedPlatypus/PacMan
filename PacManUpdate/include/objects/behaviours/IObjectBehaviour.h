f406f803528fe7e7742507accec3e21c42d9363b
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2020-01-08 06:05:58 +0100

    AB#74: Add Documentation to IObjectBehaviour

--- PacManUpdate/include/objects/behaviours/IObjectBehaviour.h
+++ PacManUpdate/include/objects/behaviours/IObjectBehaviour.h
@@ -6,10 +6,25 @@ namespace pacman {
 namespace update {
 namespace object {
 
+/// <summary>
+/// <see cref="IObjectBehaviour"/> defines the interface for object
+/// behaviours which can be executed by the <see cref="BehaviourManager"/>.
+/// </summary>
 class IObjectBehaviour {
-public:
+public:  
+  /// <summary>
+  /// Finalizes this <see cref="IObjectBehaviour"/>.
+  /// </summary>
   virtual ~IObjectBehaviour() = default;
-
+  
+  /// <summary>
+  /// Executes this <see cref="IObjectBehaviour"/>.
+  /// </summary>
+  /// <param name="p_entity">
+  /// The <see cref="IUpdatablePlayerEntity"/> on which this behaviour should act.
+  /// </param>
+  /// <param name="x"> The x location at which this behaviour is executed. </param>
+  /// <param name="y"> The y location at which this behaviour is executed. </param>
   virtual void Execute(IUpdatablePlayerEntity* p_entity, int x, int y) = 0;
 };
 
