f18c6ffd1761519f99a454df7b0058224a5269a4
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2020-01-09 20:03:26 +0100

    AB#74: Add PortalBehaviour execute at non mapped tile.

--- PacManUpdate.Test/src/objects/behaviours/PortalBehaviourTest.cpp
+++ PacManUpdate.Test/src/objects/behaviours/PortalBehaviourTest.cpp
@@ -22,7 +22,7 @@ std::unique_ptr<state::field::IField> ConstructTestField() {
   return state::field::IField::Construct(field_tiles);
 }
 
-TEST(PortalBehaviourTest, GivenAPlayerEntity_WhenPortalBehaviourIsExecuted_ThenThePlayerIsMoved) {
+TEST(PortalBehaviourTest, GivenAPlayerEntity_WhenPortalBehaviourIsExecutedAtAMappedTile_ThenThePlayerIsMoved) {
   // Given
   std::unique_ptr<state::field::IField> p_field = ConstructTestField();
 
@@ -48,6 +48,31 @@ TEST(PortalBehaviourTest, GivenAPlayerEntity_WhenPortalBehaviourIsExecuted_ThenT
   // Then
   ASSERT_THAT(p_entity_state->GetXAxis()->GetPosition(), Eq(goal_x));
   ASSERT_THAT(p_entity_state->GetYAxis()->GetPosition(), Eq(goal_y));
+}
+
+
+TEST(PortalBehaviourTest, GivenAPlayerEntity_WhenPortalBehaviourIsExecutedAtANonMappedTile_ThenThePlayerIsMoved) {
+  // Given
+  std::unique_ptr<state::field::IField> p_field = ConstructTestField();
+
+  auto initial_x = 0;
+  auto initial_y = 1;
+
+  std::unordered_map<int, int> mapping = { 
+    { p_field->GetFieldIndex(2,2), p_field->GetFieldIndex(0,1) } 
+  };
+
+  auto portal_behaviour = PortalBehaviour(p_field.get(), mapping);
+
+  auto p_entity_state = state::IEntityState::Construct(0.F, 1.F, state::Direction::Left, 30.F);
+  auto p_entity = IUpdatablePlayerEntity::Construct(p_entity_state.get());
+
+  // When
+  portal_behaviour.Execute(p_entity.get(), initial_x, initial_y);
+
+  // Then
+  ASSERT_THAT(p_entity_state->GetXAxis()->GetPosition(), Eq(initial_x));
+  ASSERT_THAT(p_entity_state->GetYAxis()->GetPosition(), Eq(initial_y));
 }
 
 }
