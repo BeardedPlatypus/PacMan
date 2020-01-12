8bfeac56e11e7dd77f1f9a503d2c3b8dc45feb50
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2020-01-12 09:34:51 +0100

    AB#50: Update FieldSpriteDefinition with new TileTypes

--- PacManState/src/field/object/FieldObjectDefinition.cpp
+++ PacManState/src/field/object/FieldObjectDefinition.cpp
@@ -2,9 +2,11 @@
 #include "field/object/FieldObjectDefinition.h"
 
 #define X FieldObjectType::Undefined
-#define _ FieldObjectType::Undefined
 #define G FieldObjectType::Undefined
 #define P FieldObjectType::Portal
+#define B FieldObjectType::BigDot
+#define _ FieldObjectType::SmallDot
+#define s FieldObjectType::Undefined
 
 
 namespace pacman {
@@ -16,24 +18,24 @@ std::vector<std::vector<FieldObjectType>> GetFieldObjectDefinition() {
     { X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X, X },
     { X, X, _, _, _, _, _, _, _, _, _, X, _, _, _, _, _, _, _, _, _, X, X },
     { X, X, _, X, X, X, _, X, X, X, _, X, _, X, X, X, _, X, X, X, _, X, X },
-    { X, X, _, X, X, X, _, X, X, X, _, X, _, X, X, X, _, X, X, X, _, X, X },
+    { X, X, B, X, X, X, _, X, X, X, _, X, _, X, X, X, _, X, X, X, B, X, X },
     { X, X, _, X, X, X, _, X, X, X, _, X, _, X, X, X, _, X, X, X, _, X, X },
     { X, X, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, X, X },
     { X, X, _, X, X, X, _, X, _, X, X, X, X, X, _, X, _, X, X, X, _, X, X },
     { X, X, _, X, X, X, _, X, _, X, X, X, X, X, _, X, _, X, X, X, _, X, X },
     { X, X, _, _, _, _, _, X, _, _, _, X, _, _, _, X, _, _, _, _, _, X, X },
-    { X, X, X, X, X, X, _, X, X, X, _, X, _, X, X, X, _, X, X, X, X, X, X },
-    { X, X, X, X, X, X, _, X, _, _, _, _, _, _, _, X, _, X, X, X, X, X, X },
-    { X, X, X, X, X, X, _, X, _, X, X, G, X, X, _, X, _, X, X, X, X, X, X },
-    { X, X, X, X, X, X, _, X, _, X, _, _, _, X, _, X, _, X, X, X, X, X, X },
-    { X, P, _, _, _, _, _, _, _, X, _, _, _, X, _, _, _, _, _, _, _, P, X },
-    { X, X, X, X, X, X, _, X, _, X, X, X, X, X, _, X, _, X, X, X, X, X, X },
-    { X, X, X, X, X, X, _, X, _, _, _, _, _, _, _, X, _, X, X, X, X, X, X },
-    { X, X, X, X, X, X, _, X, _, X, X, X, X, X, _, X, _, X, X, X, X, X, X },
-    { X, X, X, X, X, X, _, X, _, X, X, X, X, X, _, X, _, X, X, X, X, X, X },
+    { X, X, X, X, X, X, _, X, X, X, s, X, s, X, X, X, _, X, X, X, X, X, X },
+    { X, X, X, X, X, X, _, X, s, s, s, s, s, s, s, X, _, X, X, X, X, X, X },
+    { X, X, X, X, X, X, _, X, s, X, X, G, X, X, s, X, _, X, X, X, X, X, X },
+    { X, X, X, X, X, X, _, X, s, X, s, s, s, X, s, X, _, X, X, X, X, X, X },
+    { X, P, s, s, s, s, _, s, s, X, s, s, s, X, s, s, _, s, s, s, s, P, X },
+    { X, X, X, X, X, X, _, X, s, X, X, X, X, X, s, X, _, X, X, X, X, X, X },
+    { X, X, X, X, X, X, _, X, s, s, s, s, s, s, s, X, _, X, X, X, X, X, X },
+    { X, X, X, X, X, X, _, X, s, X, X, X, X, X, s, X, _, X, X, X, X, X, X },
+    { X, X, X, X, X, X, _, X, s, X, X, X, X, X, s, X, _, X, X, X, X, X, X },
     { X, X, _, _, _, _, _, _, _, _, _, X, _, _, _, _, _, _, _, _, _, X, X },
     { X, X, _, X, X, X, _, X, X, X, _, X, _, X, X, X, _, X, X, X, _, X, X },
-    { X, X, _, _, _, X, _, _, _, _, _, _, _, _, _, _, _, X, _, _, _, X, X },
+    { X, X, B, _, _, X, _, _, _, _, _, s, _, _, _, _, _, X, _, _, B, X, X },
     { X, X, X, X, _, X, _, X, _, X, X, X, X, X, _, X, _, X, _, X, X, X, X },
     { X, X, X, X, _, X, _, X, _, X, X, X, X, X, _, X, _, X, _, X, X, X, X },
     { X, X, _, _, _, _, _, X, _, _, _, X, _, _, _, X, _, _, _, _, _, X, X },
