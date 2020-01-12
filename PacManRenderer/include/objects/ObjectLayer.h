15b83adbadcb6ee35f6d9886a5cce7cdfb8546a9
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2020-01-12 10:14:26 +0100

    AB#51: Update documentation of ObjectLayer

--- PacManRenderer/include/objects/ObjectLayer.h
+++ PacManRenderer/include/objects/ObjectLayer.h
@@ -15,9 +15,20 @@ namespace renderer {
 
 using RenderMapping = std::unordered_map<state::field::FieldObjectType, std::unique_ptr<objects::IObjectRenderer>>;
 
-
+/// <summary>
+/// <see cref="ObjectLayer"/> visualises the objects contained in a 
+/// <see cref="IFieldObjectManager"/>.
+/// </summary>
+/// <seealso cref="IRenderLayer" />
 class ObjectLayer final : public IRenderLayer {
-public:
+public:  
+  /// <summary>
+  /// Create a new <see cref="ObjectLayer"/>.
+  /// </summary>
+  /// <param name="scale">The scale with which to render.</param>
+  /// <param name="p_view_api">A pointer to the <see cref="view::IViewAPI"/>.</param>
+  /// <param name="p_object_manager">A pointer to the <see cref="IFieldObjectManager"/>.</param>
+  /// <param name="p_obj_renderers">A mapping of objects to their corresponding renderers.</param>
   ObjectLayer(float scale,
               view::IViewAPI* p_view_api,
               state::field::IFieldObjectManager* p_object_manager,
@@ -25,21 +36,49 @@ public:
 
   void Initialise() final;
 
-  inline void Update(float dtime) {
+  inline void Update(float dtime) final {
     // ObjectLayer is static.
   }
 
   void Render() const final;
 
 private:
+  /// <summary>
+  /// The scale to render with
+  /// </summary>
   const float scale;
 
+  /// <summary>
+  /// Initialise the sprites of this <see cref="ObjectLayer" />.
+  /// </summary>
   void InitialiseSprites();
+  
+  /// <summary>
+  /// Determines whether this has a renderer for the specified 
+  /// <paramref name="type"/>.
+  /// </summary>
+  /// <param name="type">The type.</param>
+  /// <returns>
+  ///   <c>true</c> if [has renderer for type]; otherwise, <c>false</c>.
+  /// </returns>
   bool HasRendererForType(state::field::FieldObjectType type) const;
 
+  /// <summary>
+  /// A pointer to the <see cref="IViewManager" /> used by this 
+  /// <see cref="FieldLayer" />. 
+  /// </summary>
   view::IViewAPI* p_view_api;
+  
+  /// <summary>
+  /// A pointer to the observed <see cref="state::field::IFieldObjectManager"/>.
+  /// </summary>
   state::field::IFieldObjectManager* p_object_manager;
 
+  /// <summary>
+  /// A <see cref="std::unique_ptr"/> containing a mapping from 
+  /// <see cref="state::field::FieldObjectType"/> to its corresponding
+  /// <see cref="IObjectRenderer"/>.
+  /// </summary>
   std::unique_ptr<RenderMapping> p_render_mapping;
 };
 
