a8538b0e0e8c3f29e935ad67a2c3ae23a33c32d2
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2020-01-12 10:06:43 +0100

    AB#51: Update documentation BigDotRenderer

--- PacManRenderer/include/objects/ObjectRenderers/BigDotRenderer.h
+++ PacManRenderer/include/objects/ObjectRenderers/BigDotRenderer.h
@@ -1,12 +1,21 @@
 #pragma once
 #include "objects/ObjectRenderers/IObjectRenderer.h"
 
+
 namespace pacman {
 namespace renderer {
 namespace objects {
-
-class BigDotRenderer final : public IObjectRenderer {
-public:
+  
+/// <summary>
+/// <see cref="BigDotRenderer"/> implements <see cref="IObjectRenderer"/> for
+/// the rendering of big dots.
+/// </summary>
+/// <seealso cref="IObjectRenderer" />
+  class BigDotRenderer final : public IObjectRenderer {
+public:  
+  /// <summary>
+  /// Create a new <see cref="BigDotRenderer"/>.
+  /// </summary>
   BigDotRenderer() {}
 
   void RenderObject(view::IViewAPI* p_view_api, int x, int y, float scale) const final;
