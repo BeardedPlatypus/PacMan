4b5b2e58426307b172f7ed8f2f07b20a49b48c55
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2020-01-12 10:07:01 +0100

    AB#51: Update documentation SmallDotRenderer

--- PacManRenderer/include/objects/ObjectRenderers/SmallDotRenderer.h
+++ PacManRenderer/include/objects/ObjectRenderers/SmallDotRenderer.h
@@ -1,10 +1,16 @@
 #pragma once
 #include "objects/ObjectRenderers/IObjectRenderer.h"
 
+
 namespace pacman {
 namespace renderer {
 namespace objects {
 
+/// <summary>
+/// <see cref="SmallDotRenderer"/> implements <see cref="IObjectRenderer"/> for
+/// the rendering of small dots.
+/// </summary>
+/// <seealso cref="IObjectRenderer" />
 class SmallDotRenderer final : public IObjectRenderer {
 public:
   void RenderObject(view::IViewAPI* p_view_api, int x, int y, float scale) const final;
