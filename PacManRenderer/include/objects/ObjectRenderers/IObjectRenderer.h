afb14d22cf69fcdd8b2644fc9d3da13ed6e1f3fe
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2020-01-12 09:56:57 +0100

    AB#51: Add documentation to IObjectRenderer

--- PacManRenderer/include/objects/ObjectRenderers/IObjectRenderer.h
+++ PacManRenderer/include/objects/ObjectRenderers/IObjectRenderer.h
@@ -6,8 +6,19 @@ namespace pacman {
 namespace renderer {
 namespace objects {
 
+/// <summary>
+/// <see cref="IObjectRenderer"/> provides the interface to render a specific
+/// object with a provided <see cref="view::IViewAPI"/> at a specified location.
+/// </summary>
 class IObjectRenderer {
-public:
+public:  
+  /// <summary>
+  /// Render the corresponding object of this <see cref="IObjectRenderer"/>.
+  /// </summary>
+  /// <param name="p_view_api"> The a pointer to the <see cref="IViewAPI"/>.</param>
+  /// <param name="x"> The x tile index to render at. </param>
+  /// <param name="y"> The y tile index to render at. </param>
+  /// <param name="scale"> The scale to render with. </param>
   virtual void RenderObject(view::IViewAPI* p_view_api, int x, int y, float scale) const = 0;
 };
 
