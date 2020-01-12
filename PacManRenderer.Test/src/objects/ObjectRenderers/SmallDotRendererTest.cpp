b9ecde849810b45c68505db8f71475156d04fa5c
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2020-01-12 12:02:38 +0100

    AB#51: Add SmallDotRendererTest

---
+++ PacManRenderer.Test/src/objects/ObjectRenderers/SmallDotRendererTest.cpp
@@ -0,0 +1,44 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "objects/ObjectRenderers/SmallDotRenderer.h"
+
+#include "CommonSpriteValues.h"
+#include "objects/ObjectSpriteValues.h"
+
+#include "ViewAPIMock.h"
+
+
+namespace pacman {
+namespace renderer {
+namespace objects {
+
+TEST(SmallDotRendererTest, RenderObject_RendersViewAPI) {
+  // Setup
+  const float expected_scale = 4.F;
+  const int input_x = 5;
+  const int input_y = 3;
+
+  const float expected_x = (float)(input_x * values::tile_size + 3) * expected_scale;
+  const float expected_y = (float)(input_y * values::tile_size + 3) * expected_scale;
+
+  ViewAPIMock view_api;
+
+  EXPECT_CALL(view_api, 
+              RenderSprite(values::object_sprite_small_dot,
+                            expected_x,
+                            expected_y,
+                            expected_scale, 
+                            0.F, 
+                            false, 
+                            false)).Times(1);
+
+  auto renderer = SmallDotRenderer();
+
+  // Call | Assert
+  renderer.RenderObject(&view_api, input_x, input_y, expected_scale);
+}
+
+}
+}
+}
