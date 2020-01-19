07589a0524fead5f570df44c388b8248feaf2b40
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2020-01-19 17:35:38 +0100

    AB#107: Add IGlyphRenderer

---
+++ PacManRenderer/include/ui/font/IGlyphRenderer.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <memory>
+
+
+namespace pacman {
+namespace renderer {
+namespace ui {
+  
+/// <summary>
+/// <see cref="IGlyphRenderer"/> defines the interface with which Glyphs can be
+/// rendered.
+/// </summary>
+class IGlyphRenderer {
+public:
+  std::unique_ptr<IGlyphRenderer> Construct();
+
+  ~IGlyphRenderer() = default;
+  
+  /// <summary>
+  /// Initialises this <see cref="IGlyphRenderer"/>.
+  /// </summary>
+  virtual void Initialise() = 0;
+  
+  /// <summary>
+  /// Renders the glyph at the specified location with the specified scale.
+  /// </summary>
+  /// <param name="glyph">The glyph to be rendered.</param>
+  /// <param name="x">The x location.</param>
+  /// <param name="y">The y location.</param>
+  /// <param name="scale">The scale.</param>
+  virtual void RenderGlyph(char glyph, float x, float y, float scale) = 0;
+};
+
+}
+}
+}
