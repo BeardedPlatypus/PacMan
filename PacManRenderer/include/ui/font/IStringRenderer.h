37e542ea821951ba0f72f030b75e3cd93aa58d22
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2020-02-18 19:22:59 +0100

    AB#108: Add IStringRenderer interface

---
+++ PacManRenderer/include/ui/font/IStringRenderer.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <memory>
+#include <string>
+
+
+namespace pacman {
+namespace renderer {
+namespace ui {
+
+/// <summary>
+/// <see cref="IStringRenderer"/> definies the interface with which strings can
+/// be rendered.
+/// </summary>
+class IStringRenderer {
+public:
+  ~IStringRenderer() = default;
+  
+  /// <summary>
+  /// Renders the specified <paramref name="string"/> at the specified location
+  /// with the specified <paramref name="scale"/>.
+  /// </summary>
+  /// <param name="string">The string.</param>
+  /// <param name="x">The x.</param>
+  /// <param name="y">The y.</param>
+  /// <param name="scale">The scale.</param>
+  virtual void RenderString(std::string string, float x, float y, float scale) = 0;
+};
+
+}
+}
+}
