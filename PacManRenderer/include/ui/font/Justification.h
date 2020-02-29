587fc13b644c71a815a1016dc36ddbb7e6d4c396
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2020-02-29 10:19:27 +0100

    AB#108: Add Justification.h

---
+++ PacManRenderer/include/ui/font/Justification.h
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace pacman {
+namespace renderer {
+namespace ui {
+
+/// <summary>
+/// <see cref="Justification"/> defines the justification options with which
+/// strings can be rendered..
+/// </summary>
+enum class Justification {
+  FlushLeft,
+  Centre,
+  FlushRight
+};
+
+}
+}
+}
