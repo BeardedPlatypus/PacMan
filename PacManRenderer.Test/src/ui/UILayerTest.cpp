7c79944c7a4cd999bf5a2657792940869e0a2b12
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2020-03-01 19:13:30 +0100

    AB#108: Add UILayer.Render test

--- PacManRenderer.Test/src/ui/UILayerTest.cpp
+++ PacManRenderer.Test/src/ui/UILayerTest.cpp
@@ -2,8 +2,13 @@
 #include <gtest/gtest.h>
 
 #include <ui/UILayer.h>
+#include <ui/font/StaticStringRenderData.h>
 
 #include "StringRendererMock.h"
+
+
+using ::testing::Ref;
+
 
 namespace pacman {
 namespace renderer {
@@ -26,6 +31,33 @@ TEST(UILayerTest, Initialise_CallsStringRendererInitialise) {
   // Call
   layer.Initialise();
 }
+
+
+TEST(UILayerTest, Render_CallsStringRendererRenderString) {
+  // Setup
+  std::unique_ptr<StringRendererMock> p_string_renderer =
+    std::make_unique<StringRendererMock>();
+
+  const float expected_scale = 4.0F;
+
+  auto p_render_data =
+    std::make_unique<std::vector<std::unique_ptr<ui::IStringRenderData>>>();
+
+  auto p_data = std::make_unique<ui::StaticStringRenderData>("test string", 1.F, 2.F, 3.F, ui::Justification::Centre);
+  ui::IStringRenderData* p_data_element = p_data.get();
+  EXPECT_CALL(*p_string_renderer, RenderString(Ref(*p_data_element), expected_scale)).Times(1);
+
+  p_render_data->push_back(std::move(p_data));
+
+  UILayer layer = UILayer(expected_scale, 
+                          std::move(p_string_renderer), 
+                          std::move(p_render_data),
+                          0.F);
+
+  // Call
+  layer.Render();
+}
+
 
 }
 }
