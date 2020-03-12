114f385c75a5a0d06f0f81370b67f6c07ea0473b
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2020-03-12 20:57:55 +0100

    AB#106: Add GetSpeed test

--- PacManUpdate.Test/src/entity/UpdatablePlayerEntityTest.cpp
+++ PacManUpdate.Test/src/entity/UpdatablePlayerEntityTest.cpp
@@ -35,5 +35,22 @@ INSTANTIATE_TEST_SUITE_P(UpdatablePlayerEntityTest,
                          ActiveAxisTypeTest,
                          ::testing::ValuesIn(GetActiveAxisTypeData()));
 
+
+TEST(UpdatablePlayerEntityTest, GetSpeed_ExpectedResults) {
+  // Setup
+  const float expected_speed = 30.124F;
+  std::unique_ptr<state::IEntityState> p_entity_state = 
+    state::IEntityState::Construct(0.F, 0.F, state::Direction::Left, expected_speed);
+
+  UpdatablePlayerEntity player_entity = UpdatablePlayerEntity(p_entity_state.get());
+
+  // Call
+  float result = player_entity.GetSpeed();
+
+  // Assert
+  EXPECT_THAT(result, Eq(expected_speed));
+}
+
+
 }
 }
