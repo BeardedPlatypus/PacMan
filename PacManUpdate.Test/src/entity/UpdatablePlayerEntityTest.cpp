794cc6d206280a106e082db1361b8819c5402637
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2020-03-13 18:55:55 +0100

    AB#106: Add ActiveAxis tests

--- PacManUpdate.Test/src/entity/UpdatablePlayerEntityTest.cpp
+++ PacManUpdate.Test/src/entity/UpdatablePlayerEntityTest.cpp
@@ -52,5 +52,56 @@ TEST(UpdatablePlayerEntityTest, GetSpeed_ExpectedResults) {
 }
 
 
+TEST(UpdatablePlayerEntityTest, ActiveAxis_Consistent) {
+  // Setup
+  const float expected_speed = 30.124F;
+  std::unique_ptr<state::IEntityState> p_entity_state = 
+    state::IEntityState::Construct(0.F, 0.F, state::Direction::Left, expected_speed);
+
+  UpdatablePlayerEntity player_entity = UpdatablePlayerEntity(p_entity_state.get());
+
+  // Call
+  AxisType active_axis_type = player_entity.GetActiveAxisType();
+
+  // Assert
+  IUpdatableEntityAxis* active_axis;
+
+  IPlayerMovementAxis* active_movement_axis;
+  IPlayerMovementAxis* inactive_movement_axis;
+  if (active_axis_type == AxisType::X) {
+    active_axis = player_entity.GetXAxis();
+    active_movement_axis = player_entity.GetPlayerMovementXAxis();
+    inactive_movement_axis = player_entity.GetPlayerMovementYAxis();
+  }
+  else {
+    active_axis = player_entity.GetYAxis();
+    active_movement_axis = player_entity.GetPlayerMovementYAxis();
+    inactive_movement_axis = player_entity.GetPlayerMovementXAxis();
+  }
+
+  EXPECT_THAT(player_entity.GetActiveAxis(), Eq(active_axis));
+  EXPECT_THAT(player_entity.GetActivePlayerMovementAxis(), Eq(active_movement_axis));
+  EXPECT_THAT(player_entity.GetInactivePlayerMovementAxis(), Eq(inactive_movement_axis));
+}
+
+
+TEST(UpdatablePlayerEntityTest, SwapActiveAxis_ExpectedResults) {
+  // Setup
+  const float expected_speed = 30.124F;
+  std::unique_ptr<state::IEntityState> p_entity_state = 
+    state::IEntityState::Construct(0.F, 0.F, state::Direction::Left, expected_speed);
+
+  UpdatablePlayerEntity player_entity = UpdatablePlayerEntity(p_entity_state.get());
+
+  AxisType prev_inactive_axis_type = player_entity.GetActiveAxisType() == AxisType::X ? AxisType::Y : AxisType::X;
+
+  // Call
+  player_entity.SwapActiveAxis();
+
+  // Assert
+  EXPECT_THAT(player_entity.GetActiveAxisType(), Eq(prev_inactive_axis_type));
+}
+
+
 }
 }
