ab29973123072da1153349d06feb5848b5bf6458
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2020-03-21 17:57:50 +0100

    AB#137: Add EntityLayerTest

---
+++ PacManRenderer.Test/src/entity/EntityLayerTest.cpp
@@ -0,0 +1,68 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "entity/EntityLayer.h"
+
+#include "RenderEntityMock.h"
+
+namespace pacman {
+namespace renderer {
+
+TEST(EntityLayer, Initialise_InitialisesRenderEntities) {
+  // Setup
+  auto p_entities = std::make_unique<std::vector<std::unique_ptr<entity::IRenderEntity>>>();
+
+  for (size_t i = 0; i < 3; i++) {
+    auto p_entity = std::make_unique<RenderEntityMock>();
+    EXPECT_CALL(*(p_entity), Initialise()).Times(1);
+
+    p_entities->push_back(std::move(p_entity));
+  }
+
+  auto layer = EntityLayer(std::move(p_entities), 0.F, 0.F);
+
+  // Call | Assert
+  layer.Initialise();
+}
+
+
+TEST(EntityLayer, Update_UpdatesRenderEntities) {
+  // Setup
+  const float expected_dtime = 12.3F;
+  auto p_entities = std::make_unique<std::vector<std::unique_ptr<entity::IRenderEntity>>>();
+
+  for (size_t i = 0; i < 3; i++) {
+    auto p_entity = std::make_unique<RenderEntityMock>();
+    EXPECT_CALL(*(p_entity), Update(expected_dtime)).Times(1);
+
+    p_entities->push_back(std::move(p_entity));
+  }
+
+  auto layer = EntityLayer(std::move(p_entities), 0.F, 0.F);
+
+  // Call | Assert
+  layer.Update(expected_dtime);
+}
+
+
+TEST(EntityLayer, Render_RendersRenderEntities) {
+  // Setup
+  const float expected_scale  = 12.3F;
+  const float expected_offset = 45.6F;
+  auto p_entities = std::make_unique<std::vector<std::unique_ptr<entity::IRenderEntity>>>();
+
+  for (size_t i = 0; i < 3; i++) {
+    auto p_entity = std::make_unique<RenderEntityMock>();
+    EXPECT_CALL(*(p_entity), Render(expected_scale, expected_offset)).Times(1);
+
+    p_entities->push_back(std::move(p_entity));
+  }
+
+  auto layer = EntityLayer(std::move(p_entities), expected_scale, expected_offset);
+
+  // Call | Assert
+  layer.Render();
+}
+
+}
+}
