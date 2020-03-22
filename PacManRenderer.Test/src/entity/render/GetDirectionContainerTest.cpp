401eca49a86334adcfbe7381fde0c692d61faaba
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2020-03-22 12:34:47 +0100

    AB#137: Add GetDirectionContainer test

---
+++ PacManRenderer.Test/src/entity/render/GetDirectionContainerTest.cpp
@@ -0,0 +1,32 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <entity/render/GetDirectionContainer.h>
+
+#include "entity/IEntityState.h"
+
+using ::testing::Eq;
+
+namespace pacman {
+namespace renderer {
+namespace entity {
+namespace render {
+
+TEST(GetDirectionContainerTest, GetDirection_CallsCorrectChild) {
+  // Setup
+  state::Direction expected_direction = state::Direction::Left;
+  auto p_entity_state = state::IEntityState::Construct(0.F, 0.F, expected_direction, 0.F);
+
+  auto container = GetDirectionContainer(*p_entity_state);
+
+  // Call
+  state::Direction result = container.GetDirection();
+
+  // Assert
+  EXPECT_THAT(result, Eq(expected_direction));
+}
+
+}
+}
+}
+}
