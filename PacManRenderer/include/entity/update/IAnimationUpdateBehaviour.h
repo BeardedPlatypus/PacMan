a39000bf3b40fb52208668ebf3748d7dbd40abe6
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2020-03-27 17:32:12 +0100

    AB#137: Add IAnimationUpdateBehaviour

---
+++ PacManRenderer/include/entity/update/IAnimationUpdateBehaviour.h
@@ -0,0 +1,24 @@
+#pragma once
+
+namespace pacman {
+namespace renderer {
+namespace entity {
+namespace update {
+
+/// <summary>
+/// <see cref="IAnimationUpdateBehaviour"/> provides the method to update
+/// the associated animation with.
+/// </summary>
+class IAnimationUpdateBehaviour {
+public:  
+  /// <summary>
+  /// Updates the associated animation label with the specified time difference.
+  /// </summary>
+  /// <param name="dtime">The time difference.</param>
+  virtual void Update(float dtime) = 0;
+};
+
+}
+}
+}
+}
