eba5e8186f4091ccdf4c5758d84f66d723925579
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2020-03-29 12:49:31 +0200

    AB#137: Add AnimationRenderConfigMock

---
+++ PacManRenderer.Mock/include/AnimationRenderConfigMock.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "entity/animation/IAnimationRenderConfig.h"
+#include <gmock/gmock.h>
+
+
+class AnimationRenderConfig : public pacman::renderer::entity::animation::IAnimationRenderConfig {
+public:
+  MOCK_METHOD(std::string, GetLabel, (), (const));
+  MOCK_METHOD(void, Initialise, ());
+  MOCK_METHOD(void, Update, (float));
+};
