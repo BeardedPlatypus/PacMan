0015c8510f92e9ff7db3eee6e8c456eb1c3148fd
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2020-03-29 11:59:32 +0200

    AB#137: Add AnimationRenderConfig

---
+++ PacManRenderer/include/entity/animation/AnimationRenderConfig.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+#include <string>
+
+#include "entity/animation/IAnimationRenderConfig.h"
+#include "entity/animation/IAnimationUpdateBehaviour.h"
+#include "entity/animation/SpriteRenderConfig.h"
+
+#include "IViewAPI.h"
+
+namespace pacman {
+namespace renderer {
+namespace entity {
+namespace animation {
+
+/// <summary>
+/// <see cerf="AnimationRenderConfig"/> contains all relevant data to
+/// render a single animation.
+/// </summary>
+class AnimationRenderConfig final : public IAnimationRenderConfig {
+public:  
+  /// <summary>
+  /// Creates a new <see cref="AnimationRenderConfig"/>.
+  /// </summary>
+  /// <param name="animation_label">The animation label.</param>
+  /// <param name="animation_frame_labels">The animation frame labels.</param>
+  /// <param name="sprites">The sprite configurations.</param>
+  /// <param name="p_animation_update_behaviour">A pointer to the animation update behaviour.</param>
+  AnimationRenderConfig(view::IViewAPI* p_view_api,
+                        const std::string animation_label,
+                        float time_per_frame,
+                        const std::vector<std::string>& animation_frame_labels,
+                        const std::vector<SpriteRenderConfig> sprites,
+                        const std::unique_ptr<IAnimationUpdateBehaviour> p_animation_update_behaviour);
+  
+  std::string GetLabel() const final;
+  void Initialise() final;
+  void Update(float dtime) final;
+
+private:  
+  /// <summary>
+  /// Initialises the sprite defined with <paramref name="sprite_config" />.
+  /// </summary>
+  /// <param name="sprite_config">The sprite configuration.</param>
+  /// <remarks>
+  /// This will not call anything if a sprite with the provided name already exists.
+  /// </remarks>
+  void InitialiseSprite(const SpriteRenderConfig& sprite_config);
+  void InitialiseSpriteAnimation();
+
+  view::IViewAPI* _p_view_api;
+
+  const std::string _animation_label;
+  float _time_per_frame;
+  const std::vector<std::string> _animation_frame_labels;
+  const std::vector<SpriteRenderConfig> _sprites;
+  std::unique_ptr<IAnimationUpdateBehaviour> _p_animation_update_behaviour;
+};
+
+}
+}
+}
+}
---
+++ PacManRenderer/src/entity/animation/AnimationRenderConfig.cpp
@@ -0,0 +1,62 @@
+#include "pch.h"
+#include "entity/animation/AnimationRenderConfig.h"
+
+namespace pacman {
+namespace renderer {
+namespace entity {
+namespace animation {
+
+AnimationRenderConfig::AnimationRenderConfig(view::IViewAPI* p_view_api,
+                                             const std::string animation_label,
+                                             float time_per_frame,
+                                             const std::vector<std::string>& animation_frame_labels,
+                                             const std::vector<SpriteRenderConfig> sprites,
+                                             std::unique_ptr<IAnimationUpdateBehaviour> p_animation_update_behaviour) :
+    _p_view_api(p_view_api),
+    _animation_label(animation_label),
+    _time_per_frame(time_per_frame),
+    _animation_frame_labels(animation_frame_labels),
+    _sprites(sprites),
+    _p_animation_update_behaviour(std::move(p_animation_update_behaviour)) { }
+
+
+inline std::string AnimationRenderConfig::GetLabel() const { 
+  return this->_animation_label; 
+}
+
+
+void AnimationRenderConfig::Initialise() {
+  for (const SpriteRenderConfig& config : this->_sprites)
+    InitialiseSprite(config);
+
+  this->InitialiseSpriteAnimation();
+}
+
+
+inline void AnimationRenderConfig::InitialiseSprite(const SpriteRenderConfig& sprite_config) {
+  if (this->_p_view_api->HasSprite(sprite_config.GetSpriteLabel())) return;
+
+  this->_p_view_api->RequestSprite(sprite_config.GetSpriteLabel(),
+                                   sprite_config.GetTextureFilePath(),
+                                   sprite_config.GetX(),
+                                   sprite_config.GetY(),
+                                   sprite_config.GetW(),
+                                   sprite_config.GetH());
+}
+
+
+inline void AnimationRenderConfig::InitialiseSpriteAnimation() {
+  this->_p_view_api->RequestSpriteAnimation(this->_animation_label,
+                                            this->_time_per_frame,
+                                            this->_animation_frame_labels);
+}
+
+
+inline void AnimationRenderConfig::Update(float dtime) {
+  this->_p_animation_update_behaviour->Update(dtime);
+}
+
+}
+}
+}
+}
