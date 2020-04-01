6f91f2f544a129df4bd309628bbccaee1804d212
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2020-04-01 19:02:59 +0200

    AB#137: Add GetStateContainer test

---
+++ PacManRenderer.Test/src/entity/render/GetStateContainerTest.cpp
@@ -0,0 +1,52 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <entity/render/GetStateContainer.h>
+
+using ::testing::Eq;
+
+namespace pacman {
+namespace renderer {
+namespace entity {
+namespace render {
+
+
+enum class DummyStateEnum {
+  State1,
+  State2,
+};
+
+
+class DummyStateClass {
+public:
+  DummyStateClass(DummyStateEnum dummy_state) : 
+    _dummy_state(dummy_state) { }
+
+  DummyStateEnum GetState() const {
+    return this->_dummy_state;
+  }
+
+private:
+  DummyStateEnum _dummy_state;
+
+};
+
+
+TEST(GetStateContainerTest, GetState_ExpectedResult) {
+  // Setup
+  const DummyStateEnum expected_dummy_state = DummyStateEnum::State2;
+  auto dummy = DummyStateClass(expected_dummy_state);
+
+  auto container = GetStateContainer<DummyStateEnum>(dummy);
+
+  // Call
+  DummyStateEnum result = container.GetState();
+
+  // Assert
+  EXPECT_THAT(result, Eq(expected_dummy_state));
+}
+
+}
+}
+}
+}
