d0988337e2a3e2e95f480bf671907a468680d922
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2020-04-01 19:40:55 +0200

    AB#137: Update documentation GetDirectionContainer

--- PacManRenderer/include/entity/render/GetDirectionContainer.h
+++ PacManRenderer/include/entity/render/GetDirectionContainer.h
@@ -13,9 +13,21 @@ namespace render {
 /// </summary>
 class GetDirectionContainer final {
 public:  
+  /// <summary>
+  /// Creates a new <see cref="GetDirectionContainer"/> wrapping the provided
+  /// <paramref cref="obj"/>.
+  /// </summary>
+  /// <typeparam name="T">The type of object containing a GetDirection method.</typeparam>
+  /// <param name="obj">The object that will be wrapped.</param>
   template <typename T>
   explicit GetDirectionContainer(const T& obj): p_concept(std::make_shared<Model<T>>(std::move(obj))) {}
-
+  
+  /// <summary>
+  /// Gets the direction.
+  /// </summary>
+  /// <returns>
+  /// The direction.
+  /// </returns>
   state::Direction GetDirection() const {
     return this->p_concept->GetDirection();
   }
@@ -49,7 +61,7 @@ public:
     /// </summary>
     /// <param name="t">The object wrapped.</param>
     explicit Model(const T& t) : object(t) { }
-
+    
     state::Direction GetDirection() const final {
       return object.GetDirection();
     }
