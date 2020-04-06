9c7b5c8d61936d80d9ee269751d189ba2eae91d9
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2020-04-06 22:56:29 +0200

    AB#137: Add EntityStateTest

---
+++ PacManState.Test/src/entity/EntityStateTest.cpp
@@ -0,0 +1,46 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "entity/IEntityState.h"
+#include "SpatialStateMock.h"
+#include "entity/IPacManBehaviourState.h"
+
+#pragma region using_statements
+using ::testing::Eq;
+using ::testing::Pointee;
+using ::testing::Property;
+using ::testing::AllOf;
+#pragma endregion
+
+namespace pacman {
+namespace state {
+
+TEST(EntityStateTest, Constructor_ExpectedResults) {
+  // Setup
+  std::unique_ptr<ISpatialState> p_spatial_state =
+    std::make_unique<SpatialStateMock>();
+
+  ISpatialState* p_expected_spatial_state =
+    p_spatial_state.get();
+
+  std::unique_ptr<IPacManBehaviourState> p_behavioural_state =
+    IPacManBehaviourState::Construct(PacManStateType::Dying);
+
+  IPacManBehaviourState* p_expected_behavioural_state =
+    p_behavioural_state.get();
+
+  // Call
+  std::unique_ptr<IEntityState<IPacManBehaviourState>> p_entity_state =
+    IEntityState<IPacManBehaviourState>::Construct(std::move(p_spatial_state),
+                                                   std::move(p_behavioural_state));
+
+  // Assert
+  EXPECT_THAT(p_entity_state, 
+    Pointee(AllOf(
+      Property(&IEntityState<IPacManBehaviourState>::GetSpatialState,     Eq(p_expected_spatial_state)),
+      Property(&IEntityState<IPacManBehaviourState>::GetBehaviouralState, Eq(p_expected_behavioural_state)))));
+
+}
+
+}
+}
