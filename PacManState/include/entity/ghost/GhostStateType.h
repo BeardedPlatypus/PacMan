474e3217a542bf314b3952378548333e4791bccb
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2020-04-09 20:11:54 +0200

    AB#137: Add GhostStateType

---
+++ PacManState/include/entity/ghost/GhostStateType.h
@@ -0,0 +1,19 @@
+#pragma once
+#define DllExport __declspec( dllexport )
+
+namespace pacman {
+namespace state {
+
+/// <Summary>
+/// The different states that a Ghost can have.
+/// <summary>
+enum class DllExport GhostStateType {
+  Eaten, 
+  Edible,
+  Caged,
+  Chasing,
+  Patrolling,
+};
+
+}
+}
