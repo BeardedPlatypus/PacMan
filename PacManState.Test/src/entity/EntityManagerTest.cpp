d7588f5744ad109864a55adfd63c60bc7f8ef4c3
U-DESKTOP-VTA76BL\Monthy <mwtegelaers@gmail.com>
2020-04-10 07:10:58 +0200

    AB#137: Add entity manager GhostState tests

--- PacManState.Test/src/entity/EntityManagerTest.cpp
+++ PacManState.Test/src/entity/EntityManagerTest.cpp
@@ -25,5 +25,69 @@ TEST(EntityManagerTest, GivenAnEntityManagerAndASpatialState_WhenSetPlayerStateI
 }
 
 
+TEST(EntityManagerTest, SetBlinkyState_ExpectedResults) {
+  // Given
+  std::unique_ptr<IEntityState<IGhostBehaviourState>> p_entity_state_mock = 
+    std::make_unique<EntityStateMock<IGhostBehaviourState>>();
+  auto entity_manager = IEntityManager::Construct();
+
+  IEntityState<IGhostBehaviourState>* expected_pointer = p_entity_state_mock.get();
+
+  // When
+  entity_manager->SetBlinkyState(std::move(p_entity_state_mock));
+
+  // Then
+  EXPECT_EQ(entity_manager->GetBlinkyState(), expected_pointer);
+}
+
+
+TEST(EntityManagerTest, SetPinkyState_ExpectedResults) {
+  // Given
+  std::unique_ptr<IEntityState<IGhostBehaviourState>> p_entity_state_mock = 
+    std::make_unique<EntityStateMock<IGhostBehaviourState>>();
+  auto entity_manager = IEntityManager::Construct();
+
+  IEntityState<IGhostBehaviourState>* expected_pointer = p_entity_state_mock.get();
+
+  // When
+  entity_manager->SetPinkyState(std::move(p_entity_state_mock));
+
+  // Then
+  EXPECT_EQ(entity_manager->GetPinkyState(), expected_pointer);
+}
+
+
+TEST(EntityManagerTest, SetInkyState_ExpectedResults) {
+  // Given
+  std::unique_ptr<IEntityState<IGhostBehaviourState>> p_entity_state_mock = 
+    std::make_unique<EntityStateMock<IGhostBehaviourState>>();
+  auto entity_manager = IEntityManager::Construct();
+
+  IEntityState<IGhostBehaviourState>* expected_pointer = p_entity_state_mock.get();
+
+  // When
+  entity_manager->SetInkyState(std::move(p_entity_state_mock));
+
+  // Then
+  EXPECT_EQ(entity_manager->GetInkyState(), expected_pointer);
+}
+
+
+TEST(EntityManagerTest, SetClydeState_ExpectedResults) {
+  // Given
+  std::unique_ptr<IEntityState<IGhostBehaviourState>> p_entity_state_mock = 
+    std::make_unique<EntityStateMock<IGhostBehaviourState>>();
+  auto entity_manager = IEntityManager::Construct();
+
+  IEntityState<IGhostBehaviourState>* expected_pointer = p_entity_state_mock.get();
+
+  // When
+  entity_manager->SetClydeState(std::move(p_entity_state_mock));
+
+  // Then
+  EXPECT_EQ(entity_manager->GetClydeState(), expected_pointer);
+}
+
+
 } // state
 } // pacman
